b8cc8a8e0904d92a141ae71856ae26daf5b7347f
Emmanuel Oluwatobi <emmanueloluwatobi2000@gmail.com>
2020-09-07 04:19:26 +0100

    removed unwanted files

--- img.py
+++
@@ -1,8 +0,0 @@
-import base64
-with open("Capture6.PNG", "rb") as img:
-    res = base64.b64encode(img.read())
-    print(res)
-
-sv = open("sv_img.png", "wb")
-sv.write(res.decode('base64'))
-sv.close()
